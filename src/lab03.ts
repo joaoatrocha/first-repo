e4dedc15efc65951a1eb02b853503d82b03fc685
joaoatrocha <1222635@isep.ipp.pt>
2023-03-13 23:02:49 +0000

    exercicios

--- src/lab03.ts
+++ src/lab03.ts
@@ -1,5 +1,5 @@
-let raio: number = 0;
-let altura: number = 0;
+let raio: number = 5;
+let altura: number = 10;
 
 let volume: number = Math.PI * raio ** 2 * altura;
 let litros: number = volume / 1000;
